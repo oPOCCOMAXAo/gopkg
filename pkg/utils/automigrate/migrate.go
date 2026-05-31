6966a24f471902568816870c56e5a50a10d23f6c
POCCOMAXA <opoccomaxao@gmail.com>
2026-06-01 01:47:17 +0300

    feat: automigrate

---
+++ pkg/utils/automigrate/migrate.go
@@ -0,0 +1,51 @@
+package automigrate
+
+import (
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
+)
+
+func CreateTables(
+	migrator gorm.Migrator,
+	modelsPtrs ...any,
+) error {
+	err := migrator.AutoMigrate(modelsPtrs...)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
+}
+
+func DropTables(
+	migrator gorm.Migrator,
+	unusedTablesNames ...string,
+) error {
+	for _, table := range unusedTablesNames {
+		err := migrator.DropTable(
+			table,
+		)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+	}
+
+	return nil
+}
+
+func DropIndexesSingle(
+	migrator gorm.Migrator,
+	modelPtr any,
+	indexes ...string,
+) error {
+	for _, index := range indexes {
+		if migrator.HasIndex(modelPtr, index) {
+			err := migrator.DropIndex(modelPtr, index)
+			if err != nil {
+				return errors.WithStack(err)
+			}
+		}
+	}
+
+	return nil
+}
